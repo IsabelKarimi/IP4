871e8e2a103e21dc2ce8eca9833fa2f039481ebb
Isabel Karimi <100405131+IsabelKarimi@users.noreply.github.com>
2022-03-21 13:28:25 +0300

    updated js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    //form function to submit the users input, calculate total for the order, and prompt the user for some details
+   
     $("#text-center").submit(function(event) {
         //functions to get user input from the forms
         function flavor() {
